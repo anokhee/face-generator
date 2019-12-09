496449842cd7d220b4548e5c4852ff26493d127e
Anokhee Jandhyala <anokheejandhyala@Anokhees-MacBook-Pro.local>
2019-12-09 01:31:45 -0500

    Adds dat.gui

--- responsive_web_version/empty-example/sketch.js
+++ responsive_web_version/empty-example/sketch.js
@@ -1,95 +1,110 @@
+let face;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  face = new Face();
+
+  // let gui_head = new dat.GUI();
+  // gui_head.add(face, 'hsx', 0, 500).name("HSX");
+  // gui_head.add(face, 'hsy', -500, 500).name("HSY");
+  // gui_head.add(face, 'hcp1x', 0, 500).name("Forehead - X");
+  // gui_head.add(face, 'hcp1y', -500, 500).name("Forehead - Y");
+}
+
+// Resizes the canvas responsively
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(137, 135, 210);
   noStroke();
 
-  let hsx = windowWidth / 2 - 150;
-  let hsy = windowHeight / 2;
-  let hcp1x = windowWidth / 2 - 120;
-  let hcp1y = windowHeight / 2 + 250;
-  let hcp2x = windowWidth / 2 + 120;
-  let hcp2y = windowHeight / 2 + 250;
-  let hex = windowWidth / 2 + 150;
-  let hey = windowHeight / 2;
-
-  let bun1x = 0;
-  let bun2x = 0;
-  let buny = height / 2 - 50;
-  let bunSize = 10;
-
-  let hairk = 15;
-  let hairl = 8;
-  let hairln = windowHeight/2 - windowHeight/8;
-
-
-  let espac = 50;
-  let eypos = windowHeight / 2 + 40;
-  let ew = 60;
-  let eh = 30;
-
-  let p = 30;
-
-  let chSpacing = 80;
-  let chYpos = windowHeight / 2 - 175;
-  let ch = 50;
-
-  let noseX = 20;
-  let noseY = windowHeight / 2 + 105;
-  let noseCx = 25;
-  let noseCy = 80;
-
-  let mouthX = 40;
-  let mouthY = windowHeight / 2 + 125;
-  let mouthCx = 250 / 4;
-  let mouthCy = windowHeight / 2 + 150;
-
   strokeWeight(5);
   stroke(64, 39, 33);
   fill(134, 109, 103)
-  for (i = bunSize; i > 0; i = i - 1) {
-    ellipse(hsx + bun1x, buny, i * i, i * i); 
-    ellipse(hex + bun2x, buny, i * i, i * i); 
+  for (i = face.bunSize; i > 0; i = i - 1) {
+    ellipse(windowWidth / 2 - face.hsx + face.bun1x,
+      windowHeight / 2 - face.buny,
+      i * i, i * i);
+    ellipse(windowWidth / 2 + face.hsx + face.bun2x,
+      windowHeight / 2 - face.buny,
+      i * i, i * i);
   }
 
   fill(207, 182, 153);
-  ellipse(windowWidth / 2, windowHeight / 2, hex - hsx - 2, hsy - hey/2);
-  bezier(hsx, hsy, hcp1x, hcp1y, hcp2x, hcp2y, hex, hey);
+  bezier(windowWidth / 2 - face.hsx, windowHeight / 2 + face.hsy,
+    windowWidth / 2 - face.hcp1x, windowHeight / 2 + face.hcp1y,
+    windowWidth / 2 + face.hcp1x, windowHeight / 2 + face.hcp1y,
+    windowWidth / 2 + face.hsx, windowHeight / 2 + face.hsy);
 
   fill(134, 109, 103);
-  for (i = 0; i < hairk; i = i + 2) {
-    bezier(hsx, windowHeight/2 + i * hairl, hsx, windowHeight/4 + i * i, windowWidth/2, windowHeight/2.5, windowWidth / 2, hairln);
-    bezier(hex, windowHeight/2 + i * hairl, hex, windowHeight/4 + i * i, windowWidth/2, windowHeight/2.5, windowWidth / 2, hairln);
+  for (i = 0; i < face.hairk; i = i + 2) {
+    bezier(windowWidth / 2 - face.hsx, windowHeight / 2 + i * face.hairl, width / 2 - face.hsx, windowHeight / 4 + i * i, windowWidth / 2, windowHeight / 2.5, windowWidth / 2, windowHeight / 2 - windowHeight / 8 + face.hairln);
+    bezier(windowWidth / 2 + face.hsx, windowHeight / 2 + i * face.hairl, width / 2 + face.hsx, windowHeight / 4 + i * i, windowWidth / 2, windowHeight / 2.5, windowWidth / 2, windowHeight / 2 - windowHeight / 8 + face.hairln);
   }
-
-  fill(255);
 
   noStroke();
   fill(191, 122, 148);
-  ellipse(hsx + 130 - chSpacing, 250 + chYpos, ch, ch);
-  ellipse(hex - 130 + chSpacing, 250 + chYpos, ch, ch);
-  fill(255, 255, 255);
+  ellipse(windowWidth / 2 - face.hsx + face.chSpacing, windowHeight / 2 + face.chYpos, face.ch, face.ch);
+  ellipse(windowWidth / 2 + face.hsx - face.chSpacing, windowHeight / 2 + face.chYpos, face.ch, face.ch);
 
+
+
+  fill(255);
   stroke(64, 39, 33);
-  ellipse(windowWidth / 2 - espac, eypos, ew, eh);
-  ellipse(windowWidth / 2 + espac, eypos, ew, eh);
+  ellipse(windowWidth / 2 - face.espac, windowHeight / 2 + face.eypos, face.ew, face.eh);
+  ellipse(windowWidth / 2 + face.espac, windowHeight / 2 + face.eypos, face.ew, face.eh);
 
   fill(64, 39, 33);
-  ellipse(windowWidth / 2 - espac, eypos, p, p);
-  ellipse(windowWidth / 2 + espac, eypos, p, p);
+  ellipse(windowWidth / 2 - face.espac, windowHeight / 2 + face.eypos, face.p, face.p);
+  ellipse(windowWidth / 2 + face.espac, windowHeight / 2 + face.eypos, face.p, face.p);
 
   noFill();
-  bezier(windowWidth / 2 - mouthX, mouthY, windowWidth / 2 - mouthCx, mouthCy, windowWidth / 2 + mouthCx, mouthCy, windowWidth / 2 + mouthX, mouthY);
+  bezier(windowWidth / 2 - face.mouthX, windowHeight / 2 + face.mouthY,
+    windowWidth / 2 - face.mouthCx, windowHeight / 2 + face.mouthCy,
+    windowWidth / 2 + face.mouthCx, windowHeight / 2 + face.mouthCy,
+    windowWidth / 2 + face.mouthX, windowHeight / 2 + face.mouthY);
 
-  bezier(windowWidth / 2 - noseX, noseY,
-    windowWidth / 2 - noseCx, windowHeight / 2 + noseCy,
-    windowWidth / 2 + noseCx, windowHeight / 2 + noseCy,
-    windowWidth / 2 + noseX, noseY);
+  bezier(windowWidth / 2 - face.noseX, windowHeight / 2 + face.noseY,
+    windowWidth / 2 - face.noseCx, windowHeight / 2 + face.noseCy,
+    windowWidth / 2 + face.noseCx, windowHeight / 2 + face.noseCy,
+    windowWidth / 2 + face.noseX, windowHeight / 2 + face.noseY);
 }
 
-// Resizes the canvas responsively
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function Face() {
+  this.hsx = 150;
+  this.hsy = 0;
+  this.hcp1x = 120;
+  this.hcp1y = 250;
+
+  this.bun1x = 0;
+  this.bun2x = 0;
+  this.buny = 50;
+  this.bunSize = 10;
+
+  this.hairk = 15;
+  this.hairl = 8;
+  this.hairln = 0;
+
+  this.espac = 50;
+  this.eypos = 40;
+  this.ew = 60;
+  this.eh = 30;
+
+  this.p = 30;
+
+  this.chSpacing = 60;
+  this.chYpos = 100;
+  this.ch = 50;
+
+  this.mouthX = 40;
+  this.mouthY = 125;
+  this.mouthCx = 250 / 4;
+  this.mouthCy = 150;
+
+  this.noseX = 20;
+  this.noseY = 105;
+  this.noseCx = 25;
+  this.noseCy = 80;
 }
